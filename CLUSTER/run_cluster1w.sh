ea578ad8335752fba4961827ff4ff520afa3d015
ManghoHiller <hugo.leguillier@gmail.com>
2023-10-25 15:30:46 +0200

    upadtae cluster region in CLUSTER file

--- CLUSTER/run_cluster1w.sh
+++ CLUSTER/run_cluster1w.sh
@@ -3,7 +3,7 @@
 #création de Cluster avec 2 nœuds (1 nœud maître + 1 nœud travailleur) : dans google cloud
 
 gcloud dataproc clusters create cluster-a35a --enable-component-gateway \
---region europe-west1 --zone europe-west1-c \
+--region europe-west1 --zone europe-west1 \
 --master-machine-type n1-standard-4 --master-boot-disk-size 500 \
 --num-workers 1 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 \
 --image-version 2.0-debian10 --project projetbigdata2024
--- CLUSTER/run_cluster3w.sh
+++ CLUSTER/run_cluster3w.sh
@@ -3,7 +3,7 @@
 #création de Cluster avec 4 nœuds (1 nœud maître + 3 nœuds travailleurs) : dans google cloud
 
 gcloud dataproc clusters create cluster-a35a --enable-component-gateway \
---region europe-west1 --zone europe-west1-c \
+--region europe-west1 --zone europe-west1 \
 --master-machine-type n1-standard-4 --master-boot-disk-size 500 \
 --num-workers 3 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 \
 --image-version 2.0-debian10 --project projetbigdata2024
