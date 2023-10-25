ddfa81bfd8ba18f810f8a33dd90029c0c96d47df
ManghoHiller <hugo.leguillier@gmail.com>
2023-10-25 17:49:20 +0200

    rename run_cluster1w.sh to run_cluster2w.sh

--- CLUSTER/run_cluster2w.sh
+++ CLUSTER/run_cluster2w.sh
@@ -5,5 +5,5 @@
 gcloud dataproc clusters create cluster-a35a --enable-component-gateway \
 --region europe-west4 --zone europe-west4-c \
 --master-machine-type n1-standard-4 --master-boot-disk-size 500 \
---num-workers 1 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 \
+--num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 \
 --image-version 2.0-debian10 --project projetbigdata2024
